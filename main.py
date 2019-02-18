8547b8a97ada35b6bb89939872aa916439732060
pedro cesar luis busto <pclbusto@gmail.com>
2019-02-17 23:23:51 -0300

    test presion de teclas

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ class Wheel:
         index = 0;
         for nombre_imagen in self.lista_nombre_imagenes_titulos:
             imagen_titulo = self.get_image(os.path.join(self.path_titulos, nombre_imagen))
-            self.lista_titulos.append(Titulo(imagen_titulo, (index,800)))
-            index+=200
+            # imagen_titulo = imagen_titulo.convert()
+            self.lista_titulos.append(Titulo(imagen_titulo, (600, index)))
+            index += imagen_titulo.get_height()
 
     def get_image(self, path):
         global _image_library
@@ -34,30 +35,44 @@ class Wheel:
 
 
 if __name__ == "__main__":
-    w = Wheel()
     pygame.init()
-    screen = pygame.display.set_mode((1920, 1080))
+    screen = pygame.display.set_mode((int (pygame.display.Info().current_w/2), int(pygame.display.Info().current_h/2)))
+
+    w = Wheel()
     done = False
     clock = pygame.time.Clock()
     angle = 0
     y = 0
     x = 0
+    imagen_titulo = w.lista_titulos[3].image_titulo
+    center = imagen_titulo.get_rect().center
+    center = (center[0],center[1])
+    scale=1
+    print(center)
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
         screen.fill((0, 255, 255))
-        angle -= 0.01
-        # scale +=0.001
+        angle -= 5%360
+        scale -=0.01
         # ensure angle does not increase indefinitely
         # angle %= 360
         # create a new, rotated Surface
 
         for imagen_titulo in w.lista_titulos:
-            imagen_titulo.pos = (imagen_titulo.pos[0]+2, imagen_titulo.pos[1])
-            screen.blit(imagen_titulo.image_titulo, imagen_titulo.pos)
+            pos = (imagen_titulo.pos[0], imagen_titulo.pos[1]+300)
+            screen.blit(imagen_titulo.image_titulo, pos)
+
+        events = pygame.event.get()
+        for event in events:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_UP]:
+                print("Arriba")
+            if keys[pygame.K_DOWN]:
+                print("Abajo")
 
         pygame.display.flip()
-        clock.tick(120)
+        clock.tick(60)
 

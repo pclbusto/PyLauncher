e9e01cee2bdeb84aff57527f3c344a776fc60c10
pclbusto <pcbusto@gmail.com>
2019-08-21 23:14:50 -0300

    agregado de cambio de tamaño. sin funcionar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import  math
 
 _image_library = {}
 
@@ -13,15 +14,24 @@ class Titulo:
 class Wheel:
 
     def __init__(self):
+        # calculamos los valores de una circunferencia y guardamos los pares X,Y
+        self.lista_valores = []
+        self.radio = 100
+        for index in range(0, 1000, 1):
+            x = 10*(index / 1000)
+            y = math.sqrt(self.radio - math.pow(x, 2))*15
+            self.lista_valores.append((x,y))
+        print(self.lista_valores)
+
         #cargamos todas las imagenes del directorio
-        self.path_titulos = "plataforma/snes/titles"
+        self.path_titulos = "plataforma\\snes\\titles"
         self.lista_nombre_imagenes_titulos = os.listdir(self.path_titulos)
         self.lista_titulos = []
-        index = 0;
-        for nombre_imagen in self.lista_nombre_imagenes_titulos:
+        #index = 0
+        for i,nombre_imagen in enumerate(self.lista_nombre_imagenes_titulos):
             imagen_titulo = self.get_image(os.path.join(self.path_titulos, nombre_imagen))
-            self.lista_titulos.append(Titulo(imagen_titulo, (index,800)))
-            index+=200
+            self.lista_titulos.append(Titulo(imagen_titulo, self.lista_valores[i]))
+            #index+=200
 
     def get_image(self, path):
         global _image_library
@@ -35,29 +45,39 @@ class Wheel:
 
 if __name__ == "__main__":
     w = Wheel()
+    #os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
     pygame.init()
-    screen = pygame.display.set_mode((1920, 1080))
+    screen = pygame.display.set_mode( (800, 600),pygame.DOUBLEBUF)
     done = False
     clock = pygame.time.Clock()
     angle = 0
     y = 0
     x = 0
+    i = 0
     while not done:
+        i=i+1
+        i=i%(500-len(w.lista_titulos))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
-        screen.fill((0, 255, 255))
-        angle -= 0.01
+        screen.fill((0, 0, 0))
+        # angle -= 0.01
         # scale +=0.001
         # ensure angle does not increase indefinitely
         # angle %= 360
         # create a new, rotated Surface
 
-        for imagen_titulo in w.lista_titulos:
-            imagen_titulo.pos = (imagen_titulo.pos[0]+2, imagen_titulo.pos[1])
-            screen.blit(imagen_titulo.image_titulo, imagen_titulo.pos)
+        for index,imagen_titulo in enumerate(w.lista_titulos):
+            imagen_titulo.pos = (200, w.lista_valores[i+index][1])
+            old_size = imagen_titulo.image_titulo.get_size()
+            new_size = (int(old_size[0]*w.lista_valores[i+index][0]),int(old_size[1]*w.lista_valores[i+index][0]))
+            print("viejo tama {}".format(old_size))
+            print("Nuevo tama {}".format(new_size))
+            print(i)
+            image_res = pygame.transform.scale(imagen_titulo.image_titulo,new_size)
+            screen.blit(image_res, imagen_titulo.pos)
 
         pygame.display.flip()
-        clock.tick(120)
-
+        clock.tick(60)
